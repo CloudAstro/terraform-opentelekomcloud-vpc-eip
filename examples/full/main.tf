eb479bb74b62eaf8d5911da6f39de6fbec4da307
dibranica <erjon.dibrani@cloudastro.de>
2026-06-17 17:00:25 +0200

    feat: bump tf and provider version

--- examples/full/main.tf
+++ examples/full/main.tf
@@ -1,11 +1,13 @@
 module "vpc" {
-  source = "CloudAstro/vpc/opentelekomcloud"
-  name   = "vpc-example"
-  cidr   = "10.10.0.0/24"
+  source  = "CloudAstro/vpc/opentelekomcloud"
+  version = "1.1.0"
+  name    = "vpc-example"
+  cidr    = "10.10.0.0/24"
 }
 
 module "subnet" {
   source     = "CloudAstro/vpc-subnet/opentelekomcloud"
+  version    = "1.1.0"
   name       = "snet-example"
   cidr       = "10.10.0.0/26"
   gateway_ip = "10.10.0.1"
@@ -14,6 +16,7 @@ module "subnet" {
 
 module "elb" {
   source        = "CloudAstro/elastic-loadbalancer/opentelekomcloud"
+  version       = "1.1.0"
   vip_subnet_id = module.subnet.vpc_subnet.subnet_id
 }
 
